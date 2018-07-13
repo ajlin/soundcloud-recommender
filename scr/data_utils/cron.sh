57c7c20443e4e8713a46b413617537e95fcb332d
Ubuntu <ubuntu@ip-172-31-20-139.us-west-2.compute.internal>
2018-07-13 17:51:46 +0000

    python3.6 required for fstrings... versioning issue

--- scr/data_utils/cron.sh
+++ scr/data_utils/cron.sh
@@ -1,1 +1,1 @@
-python3 /home/ubuntu/capstone/soundcloud-recommender/scr/data_utils/crawl.py && echo "scrape at: $(date)" >> /home/ubuntu/capstone/soundcloud-recommender/scr/data_utils/logs/crawler.log
+python3.6 /home/ubuntu/capstone/soundcloud-recommender/scr/data_utils/crawl.py && echo "scrape at: $(date)" >> /home/ubuntu/capstone/soundcloud-recommender/scr/data_utils/logs/crawler.log
