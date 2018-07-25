d4d11a88c033c7078e08287dd416f418408798c4
alin <alin@local>
2018-07-25 16:47:46 -0700

    cleaning empty strings take 6

--- scr/data_utils/aws/aws.py
+++ scr/data_utils/aws/aws.py
@@ -102,7 +102,7 @@ class put(table):
 """df transformation helpers"""
 def clean(df):
     t = df.drop_duplicates()
-    t = df.replace('',"empty") #replace empty strings with 'empty'
+    t = df.replace("[^A-Za-z0-9.:/ +\n]+",'',regex=True)
     t = df.dropna(how='all')
     t = t.reindex()
     return t
